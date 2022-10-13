dab7344a3df6e044b563b368065cdb0cd72c87b0
Lyubomir <lyubomirbelov5178@gmail.com>
2022-10-13 23:21:58 +0300

    DZ

--- Program.cs
+++ Program.cs
@@ -384,38 +384,78 @@ int[,] array = new int[4, 4];
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-void PrintData(int[] inputArray)
-{
-    for (int i = 0; i < inputArray.Length; i++)
-        Console.Write($"{inputArray[i]} ");
-    Console.WriteLine();
-}
+// void PrintData(int[] inputArray)
+// {
+//     for (int i = 0; i < inputArray.Length; i++)
+//         Console.Write($"{inputArray[i]} ");
+//     Console.WriteLine();
+// }
 
-int[] GetData(string line)
+// int[] GetData(string line)
+// {
+//     Console.WriteLine(line);
+//     string inputArray = Console.ReadLine() ?? "";
+//     string[] numberArray = inputArray.Split(", ");
+//     int[] numbers = new int[numberArray.Length];
+//     bool correctInput = false;
+//     for (int i = 0; i < numberArray.Length; i++)
+//         correctInput = int.TryParse(numberArray[i], out numbers[i]);
+//     if (!correctInput)
+//         Console.WriteLine("Введите корректное число");
+//     return numbers;
+// }
+
+// void PositiveNumbers(int[] inputArr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < inputArr.Length; i++)
+//     {
+//         if (inputArr[i] > 0)
+//             sum++;
+//     }
+//     Console.WriteLine($"чисел больше 0: {sum}");
+// }
+
+// int[] arr = GetData("Введите числа через запятую: ");
+// PrintData(arr);
+// PositiveNumbers(arr);
+/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double[] GetData(string line)
 {
     Console.WriteLine(line);
     string inputArray = Console.ReadLine() ?? "";
-    string[] numberArray = inputArray.Split(", ");
-    int[] numbers = new int[numberArray.Length];
+    string[] numberArray = inputArray.Split(',');
+    double[] numbers = new double[numberArray.Length];
     bool correctInput = false;
     for (int i = 0; i < numberArray.Length; i++)
-        correctInput = int.TryParse(numberArray[i], out numbers[i]);
+        correctInput = double.TryParse(numberArray[i], out numbers[i]);
     if (!correctInput)
         Console.WriteLine("Введите корректное число");
     return numbers;
 }
-
-void PositiveNumbers(int[] inputArr)
+void PrintData(double[] arr)
 {
-    int sum = 0;
-    for (int i = 0; i < inputArr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        if (inputArr[i] > 0)
-            sum++;
+        Console.Write($"{arr[i]}");
     }
-    Console.WriteLine($"чисел больше 0: {sum}");
+    Console.WriteLine();
 }
 
-int[] arr = GetData("Введите числа через запятую: ");
-PrintData(arr);
-PositiveNumbers(arr);
+void Intersection(double[] inputArray)
+{
+    double bOne = inputArray[0];
+    double kOne = inputArray[1];
+    double bTwo = inputArray[2];
+    double kTwo = inputArray[3];
+    double x = (bTwo - bOne) / (kOne - kTwo);
+    double y = (kOne * x) + bOne;
+    Console.WriteLine($"координаты точки пересечения: ({x},{y})");
+}
+
+double[] coef = GetData("Введите значения b1, k1, b2, k2 через запятую: ");
+PrintData(coef);
+Intersection(coef);
